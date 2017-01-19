43165112214d23239048129dd442226a0738a382
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-01-19 12:17:58 +0200

    Refactored `TranslationBehavior`
    - Added `languageColumn` filed
    - Added docs
    - Fixed code style

--- behaviors/TranslationBehavior.php
+++ behaviors/TranslationBehavior.php
@@ -1,23 +1,25 @@
 <?php
 namespace bl\multilang\behaviors;
 
-use bl\multilang\entities\Language;
 use Yii;
 use yii\base\Behavior;
 use yii\db\ActiveRecordInterface;
 
+use bl\multilang\entities\Language;
+
 /**
  * Class SeoDataBehavior
- * @author Gutsulyak Vadim <guts.vadim@gmail.com>
- * @package bl\multilang\behaviors
  *
+ * Configuration example:
+ * Add this behavior to your ActiveRecord model
  * ```php
  *  public function behaviors() {
  *      return [
  *              'translation' => [
  *              'class' => TranslationBehavior::className(),
  *              'translationClass' => ArticleTranslation::className(),
- *              'relationColumn' => 'article_id'
+ *              'relationColumn' => 'articleId'
+ *              'languageColumn' => 'languageId'
  *          ]
  *      ];
  *  }
@@ -25,26 +27,46 @@ use yii\db\ActiveRecordInterface;
  * @property ActiveRecordInterface $owner
  * @property string $translationClass
  * @property string $relationColumn
+ * @property string $languageColumn
+ *
+ * @author Gutsulyak Vadim <guts.vadim@gmail.com>
+ * @package bl\multilang\behaviors
  */
 class TranslationBehavior extends Behavior
 {
+    /**
+     * @var string
+     */
     public $translationClass;
+    /**
+     * @var string
+     */
     public $relationColumn;
+    /**
+     * @var string
+     */
+    public $languageColumn = 'language_id';
 
-    public function getTranslation($languageId = null) {
+
+    /**
+     * Method for getting translation from database
+     * 
+     * @param null|integer $languageId
+     */
+    public function getTranslation($languageId = null)
+    {
         /* @var $modelClass ActiveRecordInterface */
-        /* @var $language ActiveRecordInterface */
-        /* @var $translation ActiveRecordInterface */
-
         $modelClass = $this->translationClass;
 
         if(!empty($languageId)) {
-            $language = Language::findOne($languageId);
-            if($language) {
+            /* @var $language ActiveRecordInterface */
+            if($language = Language::findOne($languageId)) {
+                /* @var $translation ActiveRecordInterface */
                 $translation = $modelClass::findOne([
-                    'language_id' => $language->getPrimaryKey(),
+                    $this->languageColumn => $language->getPrimaryKey(),
                     $this->relationColumn => $this->owner->getPrimaryKey()
                 ]);
+                
                 return $translation;
             }
         }
@@ -52,8 +74,8 @@ class TranslationBehavior extends Behavior
         $language = Language::findOne(['lang_id' => Yii::$app->language]);
 
         // try to find translation on current language
-        $translation =  $modelClass::findOne([
-            'language_id' => $language->getPrimaryKey(),
+        $translation = $modelClass::findOne([
+            $this->languageColumn => $language->getPrimaryKey(),
             $this->relationColumn => $this->owner->getPrimaryKey()
         ]);
 
@@ -63,7 +85,7 @@ class TranslationBehavior extends Behavior
 
             // try to find translation on default language
             $translation =  $modelClass::findOne([
-                'language_id' => $language->getPrimaryKey(),
+                $this->languageColumn => $language->getPrimaryKey(),
                 $this->relationColumn => $this->owner->getPrimaryKey()
             ]);
 
